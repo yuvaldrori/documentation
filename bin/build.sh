abee3186dec16ddafe332c3800c6fe4408e49ad1
Marko Locher <marko@codeship.com>
2015-09-03 12:56:26 +0200

    Deployment for the master branch

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo -e "\e[36mBuilding with base URL /${CI_BRANCH}\e[39m"
-sed -i'' -e "s/^baseurl:.*/baseurl: ${CI_BRANCH}/" _config.yml
+sed -i'' -e "s|^baseurl:.*|baseurl: /${CI_BRANCH}|" _config.yml
 bundle exec jekyll build --destination "/site/${CI_BRANCH}"
--- bin/s3.sh
+++ bin/s3.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
-# the config file is relative to the repository root
-CONFIG_FILE="_s3.json"
-aws s3api put-bucket-website --bucket "${S3_BUCKET}" --website-configuration "file://${CONFIG_FILE}"
+set -e
+action=${1:?'You need to pass an action!'} && shift
+
+case "$action" in
+	'sync')
+		echo -e "\e[36mPushing documenation to s3://${AWS_S3_BUCKET}/${CI_BRANCH}/\e[39m"
+		aws s3 sync "/site/${CI_BRANCH}/" "s3://${AWS_S3_BUCKET}/${CI_BRANCH}/" --acl public-read --follow-symlinks --delete
+		;;
+	'configure_bucket')
+		echo -e "\e[36mPushing bucket configuration\e[39m"
+		config_file=${1:?'You need to pass an action!'} && shift
+		aws s3api put-bucket-website --bucket "${AWS_S3_BUCKET}" --website-configuration "file://${config_file}"
+		;;
+	*)
+		echo -e "\e[36mInvalid action ${action} :(\e[39m"
+		exit 1
+		;;
+esac
