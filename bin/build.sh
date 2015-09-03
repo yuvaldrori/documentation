68685c87a49bbcd93c559c09eb19cfe48690ba39
Marko Locher <marko@codeship.com>
2015-09-03 11:46:33 +0200

    Build the project using Jet

---
+++ bin/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+echo -e "\e[36mBuilding with base URL /${CI_BRANCH}\e[39m"
+sed -i'' -e "s/^baseurl:.*/baseurl: ${CI_BRANCH}/" _config.yml
+bundle exec jekyll build --destination "/site/${CI_BRANCH}"
