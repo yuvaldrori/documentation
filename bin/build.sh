4a093e5b2436b016a141fdc7b2e764b49c6b9e69
Marko Locher <marko@codeship.com>
2015-09-08 10:54:08 +0200

    Further Improvements to the S3 deployment
    * Updates to .dockerignore and files excluded by the Jekyll configuration
    * Push lifecycle configuration
    * Make sure the target directory for `jekyll build` exists
      (required for branches which contain slashes)

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+destination="/site/${CI_BRANCH}"
+
 echo -e "\e[36mBuilding with base URL /${CI_BRANCH}\e[39m"
 sed -i'' -e "s|^baseurl:.*|baseurl: /${CI_BRANCH}|" _config.yml
-bundle exec jekyll build --destination "/site/${CI_BRANCH}"
+mkdir -p "${destination}"
+bundle exec jekyll build --destination "${destination}"
--- bin/s3.sh
+++ bin/s3.sh
@@ -2,18 +2,27 @@
 set -e
 action=${1:?'You need to pass an action!'} && shift
 
+log() {
+	echo -e "\e[36m$1\e[39m"
+}
+
 case "$action" in
 	'sync')
-		echo -e "\e[36mPushing documenation to s3://${AWS_S3_BUCKET}/${CI_BRANCH}/\e[39m"
+		log "Pushing documenation to s3://${AWS_S3_BUCKET}/${CI_BRANCH}/"
 		aws s3 sync "/site/${CI_BRANCH}/" "s3://${AWS_S3_BUCKET}/${CI_BRANCH}/" --acl public-read --follow-symlinks --delete
 		;;
-	'configure_bucket')
-		echo -e "\e[36mPushing bucket configuration\e[39m"
+	'configure_website')
+		log "Pushing website configuration"
 		config_file=${1:?'You need to pass an action!'} && shift
 		aws s3api put-bucket-website --bucket "${AWS_S3_BUCKET}" --website-configuration "file://${config_file}"
 		;;
+	'configure_lifecycle')
+		log "Pushing lifecycle configuration"
+		config_file=${1:?'You need to pass an action!'} && shift
+		aws s3api put-bucket-lifecycle --bucket "${AWS_S3_BUCKET}" --lifecycle-configuration "file://${config_file}"
+		;;
 	*)
-		echo -e "\e[36mInvalid action ${action} :(\e[39m"
+		log "Invalid action ${action} :("
 		exit 1
 		;;
 esac
