885dc4b8ebd7b2b311abf4a2757e819f888c8771
Marko Locher <marko@codeship.com>
2015-09-09 11:23:39 +0200

    Push a robots.txt file to the bucket root

--- bin/s3.sh
+++ bin/s3.sh
@@ -16,11 +16,15 @@ case "$action" in
 		config_file=${1:?'You need to pass an action!'} && shift
 		aws s3api put-bucket-website --bucket "${AWS_S3_BUCKET}" --website-configuration "file://${config_file}"
 		;;
-	'configure_lifecycle')
-		log "Pushing lifecycle configuration"
-		config_file=${1:?'You need to pass an action!'} && shift
-		aws s3api put-bucket-lifecycle --bucket "${AWS_S3_BUCKET}" --lifecycle-configuration "file://${config_file}"
-		;;
+		'configure_lifecycle')
+			log "Pushing lifecycle configuration"
+			config_file=${1:?'You need to pass an action!'} && shift
+			aws s3api put-bucket-lifecycle --bucket "${AWS_S3_BUCKET}" --lifecycle-configuration "file://${config_file}"
+			;;
+		'robots')
+			log "Pushing robots.txt file"
+			aws s3 cp _robots.txt "s3://${AWS_S3_BUCKET}/robots.txt" --acl public-read
+			;;
 	*)
 		log "Invalid action ${action} :("
 		exit 1
