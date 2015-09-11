9d902acba616b892034256afbedcc834c972261e
Marko Locher <marko@codeship.com>
2015-09-11 21:12:28 +0200

    Make the Jet S3 sync verbose again

--- bin/jet.sh
+++ bin/jet.sh
@@ -8,7 +8,7 @@ target="/site/.jet"
 
 log "Preparing Jet release notes and current version"
 rm -rf "${target}" && mkdir -p "${target}/sync"
-aws s3 sync "s3://${JET_RELEASE_BUCKET}/" "${target}/sync" --exclude=* --include=*.changelog --quiet
+aws s3 sync "s3://${JET_RELEASE_BUCKET}/" "${target}/sync" --exclude=* --include=*.changelog
 
 # write the release notes
 find "${target}/sync/" -type f -not -size 0 | natsort -r | \
